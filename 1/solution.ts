434c64800bd8a72b33fad161392f9f0fa02a8bdf
steean <stian.fredsted@gmail.com>
2020-12-04 22:21:03 +0100

    day 1

--- 1/solution.ts
+++ 1/solution.ts
@@ -12,18 +12,31 @@ class First {
                 numbers.push(+number);
             }
 
-            // part 1
-            for (let number of numbers) {
-                if (numbers.includes(2020-number)) {
-                    console.log(number*(2020-number));
-                    break;
-                } 
-            }
-
-            // part 2
-            // sleep - I will get back :-)
-            
+            console.log('PART 1: ', this.partOne(numbers, 2020));
+            console.log('PART 2: ', this.partTwo(numbers, 2020));
         });
+    }
+
+    private partOne(numbers: number[], target: number) {
+        for (let number of numbers) {
+            const rest = target - number;
+            if (numbers.includes(rest)) {
+                return number*rest;
+            } 
+        }
+
+        return 0;
+    }
+
+    private partTwo(numbers: number[], target: number) {
+        for (let i = 0; i < numbers.length; i++) {
+            const rest = target - numbers[i];
+            let firstMultiplication = this.partOne(numbers.slice(i+1, numbers.length), rest);
+
+            if (firstMultiplication) {
+                return numbers[i] * firstMultiplication;
+            }
+        }
     }
 }
 
